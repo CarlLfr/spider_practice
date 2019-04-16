02cabc59555729c1ef7db21c845c3a3fbe595699
CarlLiu <490635974@qq.com>
2019-04-16 18:07:45 +0800

    百度贴吧图片爬取

--- baidutiebaSpider_test.py
+++ baidutiebaSpider_test.py
@@ -5,24 +5,25 @@ from urllib import parse
 from urllib import request
 from lxml import etree
 
-class Spider(object):
 
+class Spider(object):
     def __init__(self):
         self.tiebaName = "java"
         self.beginPage = 1
         self.endPage = 3
         self.url = "http://tieba.baidu.com/f?"
-        self.ua_header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"}
+        self.ua_header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"}
         self.pageName = 1
         # self.fileName = r"F:\image"
 
     # 构造java页面url
     def tiebaSpider(self):
-        for page in range(self.beginPage, self.endPage+1):
-            pn = (page-1)*50
+        for page in range(self.beginPage, self.endPage + 1):
+            pn = (page - 1) * 50
             wo = {"kw": self.tiebaName, "pn": pn}
             word = parse.urlencode(wo)
-            my_url = self.url+word
+            my_url = self.url + word
             self.loadPage(my_url)
 
     # 获取页面内容
@@ -33,32 +34,33 @@ class Spider(object):
         html = etree.HTML(result)
         links = html.xpath('//div[@class="threadlist_lz clearfix"]/div/a/@href')
         print(links)
-        # for link in links:
-        #     link = self.url+link
-        #     self.loadImage(link)
+        for link in links:
+            link = self.url + link
+            self.loadImage(link)
 
     # 爬取帖子详情，获取图片的链接
-    # def loadImage(self, link):
-    #     req = request.Request(link, headers=self.ua_header)
-    #     result = request.urlopen(req).read().decode()
-    #
-    #     html = etree.HTML(result)
-    #     links = html.xpath('//img[@class="BDE_Image"]/@src')
-    #     print(links)
+    def loadImage(self, link):
+        req = request.Request(link, headers=self.ua_header)
+        result = request.urlopen(req).read().decode()
 
-    #     for link in links:
-    #         self.writeImages(link)
-    #
-    # # 通过图片所在链接，爬取图片并保存图片到本地
-    # def writeImages(self, link):
-    #     print("正在保存第%d张图片" % self.pageName)
-    #     req = request.Request(link, headers=self.ua_header)
-    #     result = request.urlopen(req).read()
-    #
-    #     with open(r"F:\image\"+str(self.pageName)+".jpg", "a") as f:
-    #         f.write(result)
-    #
-    #     self.pageName += 1
+        html = etree.HTML(result)
+        links = html.xpath('//img[@class="BDE_Image"]/@src')
+        print(links)
+
+        for link in links:
+            self.writeImages(link)
+
+    # 通过图片所在链接，爬取图片并保存图片到本地
+    def writeImages(self, link):
+        print("正在保存第%d张图片" % self.pageName)
+        req = request.Request(link, headers=self.ua_header)
+        result = request.urlopen(req).read()
+
+        with open(r"F:\image\\" + str(self.pageName) + ".jpg", "a") as f:
+            f.write(result)
+
+        self.pageName += 1
+
 
 if __name__ == '__main__':
     my_spider = Spider()
