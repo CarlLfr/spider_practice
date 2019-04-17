588a3de7e54e28ae4233dda8e63421d4546293fa
CarlLiu <490635974@qq.com>
2019-04-17 14:32:19 +0800

    百度贴吧图片爬虫，脚本调通

--- baidutiebaSpider_test.py
+++ baidutiebaSpider_test.py
@@ -1,73 +1,70 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-from urllib import parse
+# 图片爬虫
+import urllib
 from urllib import request
 from lxml import etree
 
-
 class Spider(object):
+
     def __init__(self):
         self.tiebaName = "java"
         self.beginPage = 1
-        self.endPage = 3
+        self.endPage = 2
         self.url = "http://tieba.baidu.com/f?"
-        self.ua_header = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"}
+        self.ua_header = {"User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1 Trident/5.0;"}
         self.pageName = 1
-        # self.fileName = r"F:\image"
+        # self.fileName = r"F:\image\\"
 
-    # 构造java页面url
+    # 构造url
     def tiebaSpider(self):
-        for page in range(self.beginPage, self.endPage + 1):
-            pn = (page - 1) * 50
-            wo = {"kw": self.tiebaName, "pn": pn}
-            word = parse.urlencode(wo)
-            my_url = self.url + word
+        for page in range(self.beginPage, self.endPage+1):
+            pn = (page-1)*50
+            wo = {'pn':pn, 'kw':self.tiebaName}
+            word = urllib.parse.urlencode(wo)
+            my_url = self.url+word
             self.loadPage(my_url)
+        print("下载完成！")
 
-    # 获取页面内容
-    def loadPage(self, my_url):
-        req = request.Request(my_url, headers=self.ua_header)
-        result = request.urlopen(req).read().decode()
+    # 获取页面内容，爬取页面对应帖子的href属性值，通过href属性值构造对应帖子详情页面url，即link
+    def loadPage(self, url):
+        req = request.Request(url, headers=self.ua_header)
+        data = request.urlopen(req).read()
+        # print(data)
 
-        html = etree.HTML(result)
+        html = etree.HTML(data)
         links = html.xpath('//div[@class="threadlist_lz clearfix"]/div/a/@href')
-        print(links)
+        # print(links)
         for link in links:
-            link = self.url + link
-            self.loadImage(link)
+            link = "http://tieba.baidu.com"+link
+            self.loadImages(link)
 
-    # 爬取帖子详情，获取图片的链接
-    def loadImage(self, link):
-        req = request.Request(link, headers=self.ua_header)
-        result = request.urlopen(req).read().decode()
-
-        html = etree.HTML(result)
-        links = html.xpath('//img[@class="BDE_Image"]/@src')
-        print(links)
-
-        for link in links:
-            self.writeImages(link)
-
-    # 通过图片所在链接，爬取图片并保存图片到本地
-    def writeImages(self, link):
-        print("正在保存第%d张图片" % self.pageName)
+    # 爬取帖子详情，通过爬取src属性获取图片的链接image_link
+    def loadImages(self, link):
         req = request.Request(link, headers=self.ua_header)
         result = request.urlopen(req).read()
 
-        with open(r"F:\image\\" + str(self.pageName) + ".jpg", "a") as f:
-            f.write(result)
+        html = etree.HTML(result)
+        links = html.xpath('//img[@class="BDE_Image"]/@src')
+        # print(links)
+
+        for image_link in links:
+            self.writeImages(image_link)
+
+    # 通过图片链接，爬取图片并保存图片到本地
+    def writeImages(self, image):
+        print("正在保存第%d张图片" % self.pageName)
+        req = request.Request(image, headers=self.ua_header)
+        data = request.urlopen(req).read()
+
+        # 保存图片到本地
+        with open(r"F:\image\\"+str(self.pageName)+".jpg", "wb") as f:
+            f.write(data)
+        # file = open(r"F:\image\\" + str(self.pageName) + ".jpg", "wb")
+        # file.write(data)
+        # file.close()
 
         self.pageName += 1
-
 
 if __name__ == '__main__':
     my_spider = Spider()
     my_spider.tiebaSpider()
-
-
-
-
-
 
